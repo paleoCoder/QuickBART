65e6c1cac3afb3213bedba3bcf38010fa5b683b4
Scott Truger <scott.truger@truger.net>
2011-04-16 16:16:28 -0700

    fixed time listing to show one past current time and 4 after current time

--- src/com/trugertech/quickbart/BartAPI_URIGenerator.java
+++ src/com/trugertech/quickbart/BartAPI_URIGenerator.java
@@ -1,11 +1,12 @@
 package com.trugertech.quickbart;
 
+
 public class BartAPI_URIGenerator {
 	protected static final String API_KEY = "YRSI-ZII8-QHUQ-JXYX";
 	protected static final String URI_ROOT = "http://api.bart.gov/api/";
 	
 	public static final String getCmd_Sched(String orig, String dest){
-		return URI_ROOT + "sched.aspx?cmd=arrive&orig=" + orig + "&dest=" + dest +"&key=" + API_KEY;
+		return URI_ROOT + "sched.aspx?cmd=depart&orig=" + orig + "&dest=" + dest + "&a=4&b=1&key=" + API_KEY;
 	}
 	
 	public static final String getCmd_Stn(){

17ba0c8bc0f738d14741d8fa23262e0f3847f009
Scott Truger <scott.truger@truger.net>
2011-05-27 17:55:41 -0700

    started new activity for planner

---
+++ src/com/trugertech/quickbart/ActivityPlanner.java
@@ -0,0 +1,11 @@
+package com.trugertech.quickbart;
+
+import android.app.Activity;
+
+public class ActivityPlanner extends Activity {
+	
+	//TODO: use style for layout file
+	//TODO: use inflator for layout???
+	//TODO: why is this not picked u pby git?
+
+}
